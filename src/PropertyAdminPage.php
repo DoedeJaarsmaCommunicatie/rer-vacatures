548c5e159d6a919fa46556b3be39c54e532bda6f
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-02-19 10:49:24 +0100

    Changed name to "Sollicitanten"

--- src/PropertyAdminPage.php
+++ src/PropertyAdminPage.php
@@ -12,17 +12,17 @@ class PropertyAdminPage
 	{
 		add_action('admin_menu', [$this, 'vacancy_overview']);
 	}
-	
+
 	public function vacancy_overview()
 	{
 		$page_hook = add_menu_page(
-			__('Vacature overzicht', 'ppmm'),
-			__('Vacature overzicht', 'ppmm'),
+			__('Sollicitanten', 'ppmm'),
+			__('Sollicitanten', 'ppmm'),
 			'manage_options',
 			'vacancy-overview',
 			[$this, 'vacancy_list']
 		);
-		
+
 		add_submenu_page(
 			'vacancy-overview',
 			__('Single Solicitor', 'ppmm'),
@@ -31,7 +31,7 @@ class PropertyAdminPage
 			'single_solicitor',
 			[$this, 'single_vacancy']
 		);
-		
+
 		add_submenu_page(
 			'vacancy-overview',
 			__('Open sollicitaties', 'ppmm'),
@@ -40,10 +40,10 @@ class PropertyAdminPage
 			'sollicitor-overview',
 			[$this, 'open_soll_list']
 		);
-		
+
 		add_action("load-{$page_hook}", [$this, 'add_page_options']);
 	}
-	
+
 	public function add_page_options()
 	{
 		$args = [
@@ -51,10 +51,10 @@ class PropertyAdminPage
 			'default'   => 15,
 			'options'   => 'vacatures_per_page'
 		];
-		
+
 		add_screen_option('per_page', $args);
 	}
-	
+
 	public function vacancy_list()
 	{
 		$vacancyListTable = new VacancyTableList();
@@ -72,14 +72,14 @@ class PropertyAdminPage
 		</div>
 		<?php
 	}
-	
+
 	public function single_vacancy(): void
 	{
-	    
+
 	    wp_enqueue_Script('single-vacature-app', PP_VA_URL . '/assets/dist/app.react.js', [], false, true);
 		include_once PP_VA_DIR . '/src/views/single-sollicitatie.php';
 	}
-	
+
 	public function open_soll_list()
 	{
 		$sollListTable = new SollicitorTableList();

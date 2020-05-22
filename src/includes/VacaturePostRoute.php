c81be388850b00b1906b41e487cf61c1dec6d256
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-05-22 10:41:43 +0200

    Added basic honeypot esque check

--- src/includes/VacaturePostRoute.php
+++ src/includes/VacaturePostRoute.php
@@ -8,36 +8,48 @@ use PropertyPeople\Includes\Models\Vacature;
 class VacaturePostRoute
 {
 	private $request;
-	
+
 	public function __construct()
 	{
 		add_action('admin_post_post_vacature', [$this, 'handleRequest']);
 		add_action('admin_post_nopriv_post_vacature', [$this, 'handleRequest']);
-		
+
 		add_action('admin_post_post_open_soll', [$this, 'handleOpenRequest']);
 		add_action('admin_post_nopriv_post_open_soll', [$this, 'handleOpenRequest']);
 	}
-	
+
 	public function handleRequest()
 	{
 		$this->request = $_REQUEST;
-		
+
+
+		if (strpos($_REQUEST['voornaam'], 'gutenmorgen') !== false) {
+			wp_safe_redirect(wp_get_referer());
+			return;
+		}
+
+		if (isset($_REQUEST['tussenvoegsel']) && $_REQUEST['tussenvoegsel'] !== '') {
+			wp_safe_redirect(wp_get_referer());
+			return;
+		}
+
+
 		$vacature = new Vacature($_REQUEST);
-		
+
 		if (isset($_FILES['cv'])) {
 			$cv = $_FILES['cv'];
             add_filter('upload_dir', [ $this, 'custom_upload_dir']);
             $uploadedCV = wp_handle_upload($cv, ['test_form' => false]);
-            
+
             if ($uploadedCV && !isset($uploadedCV['error'])) {
                 $month = date('m');
                 $year = date('Y');
                 $vacature->file = str_replace("/$year/$month", '', $uploadedCV['url']);
             }
-            
+
             remove_filter('upload_dir', [ $this, 'custom_upload_dir']);
 		}
-		
+
 		try {
 			$vacature->save();
 		} catch (\Exception $exception) {
@@ -62,7 +74,7 @@ class VacaturePostRoute
 		);
 
 		$text = sprintf("Beste %s, \r\n \r\n Bedankt voor je sollicitatie. Wij nemen spoedig contact met je op. \r\n \r\n Met vriendelijke groet, \r\n Real Estate Recruiters", $vacature->firstname);
-		
+
 		wp_mail(
 			$vacature->email,
 			'Bedankt voor je sollicitatie',
@@ -71,11 +83,11 @@ class VacaturePostRoute
 				'From: Real Estate Recruiters <info@realestaterecruiters.nl>'
 			]
 			);
-		
+
 		if (isset($_REQUEST['type']) && $_REQUEST['type'] === 'json') {
 			wp_send_json($vacature, 201);
 		}
-		
+
 		status_header(201);
 		wp_safe_redirect(
 			add_query_arg(
@@ -87,35 +99,35 @@ class VacaturePostRoute
 			)
 		);
 	}
-	
+
 	public function handleOpenRequest()
 	{
 		$this->request = $_REQUEST;
-		
+
 		$openVacature = new OpenVacature($this->request);
-		
+
 		if (isset($_FILES['cv'])) {
 			$cv = $_FILES['cv'];
 			add_filter('upload_dir', [ $this, 'custom_upload_dir']);
 			$uploadedCV = wp_handle_upload($cv, ['test_form' => false]);
-            
+
             if ($uploadedCV && !isset($uploadedCV['error'])) {
                 $month = date('m');
                 $year = date('Y');
                 $openVacature->file = str_replace("/$year/$month", '', $uploadedCV['url']);
             }
-			
+
 			remove_filter('upload_dir', [ $this, 'custom_upload_dir']);
 		}
-		
-		
+
+
 		try {
 			$openVacature->save();
 		} catch (\Exception $exception) {
             wp_mail(
                 'support@doedejaarsma.nl',
                 'Problemen sollicitatie',
-                $exception->getMessage()
+                $exception->getMessage(),
                 [
                     'From: Real Estate Recruiters <info@realestaterecruiters.nl>'
                 ]
@@ -133,7 +145,7 @@ class VacaturePostRoute
 		);
 
 		$text = sprintf("Beste %s, \r\n \r\n Bedankt voor je sollicitatie. Wij nemen spoedig contact met je op. \r\n \r\n Met vriendelijke groet, \r\n Real Estate Recruiters", $openVacature->firstname);
-		
+
 		wp_mail(
 			$openVacature->email,
 			'Bedankt voor je sollicitatie',
@@ -142,11 +154,11 @@ class VacaturePostRoute
 				'From: Real Estate Recruiters <info@realestaterecruiters.nl>'
 			]
 			);
-		
+
 		if (isset($_REQUEST['type']) && $_REQUEST['type'] === 'json') {
 			wp_send_json($openVacature, 201);
 		}
-		
+
 		status_header(201);
 		wp_safe_redirect(
 			add_query_arg(
@@ -158,11 +170,11 @@ class VacaturePostRoute
 			)
 		);
 	}
-	
-	
+
+
 	public function custom_upload_dir($dir_data) {
 	    $custom_dir = 'CV';
-	    
+
 	    return [
             'path' => $dir_data[ 'basedir' ] . '/' . $custom_dir,
             'url' => $dir_data[ 'url' ] . '/' . $custom_dir,
